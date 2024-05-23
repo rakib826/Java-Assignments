7a1c41f4447dc42ea28e02c785ca7d14c079fd62
Kazi Md. Rakibul Hassan <kazimdrakibulhasan@gmail.com>
2024-05-23 12:12:02 +0600

    assigmment - 1

---
+++ src/assignments/Assignment_1.java
@@ -0,0 +1,11 @@
+package assignments;
+
+public class Assignment_1 {
+    public static void main(String[] arg){
+        System.out.println("id: 101");
+        System.out.println("title: iphone15");
+        System.out.println("price: 1985 euro");
+        System.out.println("description: perfect product with best image quality");
+        System.out.println("category: phone");
+    }
+}
